2592cdeab952c475152a338b573494123da4de13
Michael Budnick <michaeldbudnick@gmail.com>
2015-10-14 12:30:40 -0400

    Add pre-req and instructor association to Course

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -5,10 +5,27 @@ class Course < ActiveRecord::Base
     foreign_key: :course_id,
     primary_key: :id
   )
+  #SELECT * FROM enrollments WHERE enrollment.course_id = ?
 
   has_many(
     :enrolled_students,
     through: :enrollments,    #call self.enrollments (WHERE enrollment.course_id = self.id)
     source: :student      #from that table, gather student id
   )
+
+  belongs_to(
+    :prerequisite,
+    class_name: "Course",
+    foreign_key: :prereq_id,
+    primary_key: :id
+  )
+
+  belongs_to(
+    :instructor,
+    class_name: "User",
+    foreign_key: :instructor_id,
+    primary_key: :id
+  )
+
+  #SELECT * FROM courses WHERE course.id = self.prereq_id
 end
