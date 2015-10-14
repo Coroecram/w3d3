7622616add36d13dd87f02a95489d8bd1279eeb6
Michael Budnick <michaeldbudnick@gmail.com>
2015-10-14 14:03:23 -0400

    New rails project 'URLShortener'

---
+++ URLShortener/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+URLShortener::Application.config.secret_key_base = '583311aaa8fcb92339f287cc1faa6c0f92392ccfa0c8554c210b63e9a018df0ca673585b642372007476eb5169476ba2dd49b69dd86b6d870a1585f60b3a969b'
